bb3e363602b41b3a4b59d90bd855a7b252a3f803
Ziwei <zguo38@emory.edu>
2016-07-07 11:39:22 -0400

    new commit

---
+++ PlotAssignment/plot3.R
@@ -0,0 +1,20 @@
+library("dplyr")
+library(lubridate)
+f <- read.table("household_power_consumption.txt",header = TRUE, sep = ";",stringsAsFactors = FALSE)
+f <- within(f, DT <- paste(Date, Time, sep = " "))
+f <- mutate(f,Date = dmy(Date))
+data <- filter(f, year(Date) == 2007, month(Date) == 02, day(Date) < 3)
+data <- mutate(data, DT = dmy_hms(DT))
+data <- mutate(data, Sub_metering_1 = as.numeric(Sub_metering_1))
+data <- mutate(data, Sub_metering_2 = as.numeric(Sub_metering_2))
+data <- mutate(data, Sub_metering_3 = as.numeric(Sub_metering_3))
+
+plot(data$DT, data$Sub_metering_1, type = "n",xlab = "", ylab = "Energy sub metering", col = "black")
+points(data$DT, data$Sub_metering_1, type = "l",xlab = "", ylab = "Energy sub metering", col = "black")
+points(data$DT, data$Sub_metering_2, type = "l",xlab = "", ylab = "Energy sub metering", col = "red")
+points(data$DT, data$Sub_metering_3, type = "l",xlab = "", ylab = "Energy sub metering", col = "blue")
+
+legend("topright", lty = 1, legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col = c("black","red","blue"))
+
+dev.copy(png,file = "plot3.png")
+dev.off()
